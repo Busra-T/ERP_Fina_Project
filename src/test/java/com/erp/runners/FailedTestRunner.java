130a885eb5d584d85f501b4aaafec5f3694470cf
Busra_Cydeo <juliebt2012@gmail.com>
2023-03-10 18:29:46 -0500

    runner classes updated

--- src/test/java/com/erp/runners/FailedTestRunner.java
+++ src/test/java/com/erp/runners/FailedTestRunner.java
@@ -1,4 +1,19 @@
 package com.erp.runners;
 
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features = "",
+        glue = "com/erp/step_definitions"
+
+)
+
 public class FailedTestRunner {
+
+
 }
--- src/test/java/com/erp/runners/RoadRunner.java
+++ src/test/java/com/erp/runners/RoadRunner.java
@@ -1,4 +1,30 @@
 package com.erp.runners;
 
-public class RoadRunner {
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+
+    @RunWith(Cucumber.class)
+    @CucumberOptions(
+            plugin = {
+                    "json:target/cucumber.json",
+                    "pretty",
+                    "html:target/cucumber-reports.html",
+                    "junit:target/junit/junit-report.xml",
+                    "rerun:target/rerun.txt",
+                    "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+            },
+            features = "src/test/resources/features",
+            glue = "com/erp/step_definitions",
+            dryRun = false,
+            tags = "",
+            publish = true
+
+    )
+
+    public class RoadRunner {
+
+
 }
